4f3db50a7ea02415a77aaa33b33a30fa457514d1
endepointe <endepointe@gmail.com>
2021-05-31 23:36:38 -0700

    touchup on dropdown

--- assets/header.js
+++ assets/header.js
@@ -1,8 +1,13 @@
-const menuBtn = document.getElementById('menu-btn');
-
+//const menuBtn = document.getElementById('menu-btn');
+const navDropdownMenu = document.getElementById('nav-dropdown-menu')
 
 
 // EVENT LISTENERS
-menuBtn.addEventListener('click', function () {
-  console.log('drop the menu')
+// menuBtn.addEventListener('click', function () {
+//   console.log('drop the menu')
+// })
+
+$('#menu-btn').click(function () {
+  console.log(navDropdownMenu.style, navDropdownMenu.offsetHeight)
+  navDropdownMenu.classList.toggle('show-dropdown-menu');
 })
