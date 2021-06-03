3188f180c87f003bae049be75a4fb4f5d52a5691
endepointe <endepointe@gmail.com>
2021-06-03 06:22:32 -0700

    adjust the slider image sizes

---
+++ assets/slideshow-custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.slides-custom').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    speed: 400,
+    adaptiveHeight: true,
+    fade: true, 
+    cssEase: 'linear' 
+  });
+});
