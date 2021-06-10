8d25953db3472ca03a877f1cb3a46379b025b551
endepointe <endepointe@gmail.com>
2021-06-10 15:13:09 -0700

    start on loading products and checkout

--- assets/slideshow-custom.js
+++ assets/slideshow-custom.js
@@ -1,22 +1,59 @@
 const slideList = document.querySelectorAll('.slideshow-img-custom');
+const hoverList = document.querySelectorAll('.hover-text');
 const slideHoverTextList = document.querySelectorAll('.hover-text');
 
 $(document).ready(function() {
+
+  window.onresize = function() {
+    console.log(window.innerWidth);
+  }
+
   $('.slides-custom').slick({
     dots: false,
     infinite: true,
-    slidesToShow: 1,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     adaptiveHeight: true,
-    fade: true, 
+    fade: false, 
     cssEase: 'linear',
     autoplay: true,
     autoplaySpeed: 3100,
     arrows: false,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          fade: false,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          fade: false,
+        }
+      },
+      {
+        breakpoint: 501,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          fade: true,
+        }
+      },
+    ]
   });
 });
-
-
 // Actions
-slideList.forEach((slide, i) => {
-  // might use for analytics to see which image the visitor clicked on
+hoverList.forEach((link, i) => {
+  if ((i+1) % 2 === 0) {
+    link.textContent = 'See more';
+  }
+  if ((i+1) % 3 === 0) {
+    link.textContent = 'Shop all';
+  }
+  console.log(link);
 });
